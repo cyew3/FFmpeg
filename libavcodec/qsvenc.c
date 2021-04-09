dfdd016c6f2c73843d3d892f71ee2924111de0ba
Chen,Wenbin <wenbin.chen@intel.com>
2021-04-09 14:00:40 +0800

    libavcodec/qsvenc: add mbbrc to hevc_qsv
    Add mbbrc to hevc_qsv
    For detailed description, please see "mbbrc" part in:
    https://github.com/Intel-Media-SDK/MediaSDK/blob/master/doc/mediasdk-man.md#mfxextcodingoption2
    
    Signed-off-by: Wenbin Chen <wenbin.chen@intel.com>

--- libavcodec/qsvenc.c
+++ libavcodec/qsvenc.c
@@ -739,8 +739,6 @@ static int init_video_param(AVCodecContext *avctx, QSVEncContext *q)
 
             if (q->bitrate_limit >= 0)
                 q->extco2.BitrateLimit = q->bitrate_limit ? MFX_CODINGOPTION_ON : MFX_CODINGOPTION_OFF;
-            if (q->mbbrc >= 0)
-                q->extco2.MBBRC = q->mbbrc ? MFX_CODINGOPTION_ON : MFX_CODINGOPTION_OFF;
 
             if (q->max_frame_size >= 0)
                 q->extco2.MaxFrameSize = q->max_frame_size;
@@ -791,6 +789,9 @@ static int init_video_param(AVCodecContext *avctx, QSVEncContext *q)
                 q->extco2.MaxQPP = q->extco2.MaxQPB = q->extco2.MaxQPI;
             }
 #endif
+            if (q->mbbrc >= 0)
+                q->extco2.MBBRC = q->mbbrc ? MFX_CODINGOPTION_ON : MFX_CODINGOPTION_OFF;
+
             q->extco2.Header.BufferId = MFX_EXTBUFF_CODING_OPTION2;
             q->extco2.Header.BufferSz = sizeof(q->extco2);
 
